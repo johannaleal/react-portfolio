30a803d1acda29b9f7abb8cccaeb62317bc3c2b6
Johanna L <johannarleal@gmail.com>
2021-03-25 16:41:02 -0400

    Added folder and file structure

---
+++ src/pages/About.js
@@ -0,0 +1,14 @@
+import React from "react";
+// import Hero from "../components/Hero";
+// import Container from "../components/Container";
+// import Row from "../components/Row";
+// import Col from "../components/Col";
+
+function About() {
+  return (
+      <div>About</div>
+  );
+
+};
+
+export default About;
---
+++ src/pages/Contact.js
@@ -0,0 +1,14 @@
+import React from "react";
+// import Hero from "../components/Hero";
+// import Container from "../components/Container";
+// import Row from "../components/Row";
+// import Col from "../components/Col";
+
+function Contact() {
+  return (
+      <div>Contact</div>
+  );
+
+};
+
+export default Contact;
