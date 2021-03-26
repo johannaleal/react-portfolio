d388a0cd1f8e339bbfd80eba4edfcdcaac7ba352
Johanna L <johannarleal@gmail.com>
2021-03-26 01:35:29 -0400

    Added About page code

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,14 +1,41 @@
 import React from "react";
-// import Hero from "../components/Hero";
-// import Container from "../components/Container";
-// import Row from "../components/Row";
-// import Col from "../components/Col";
 
 function About() {
   return (
-      <div>About</div>
-  );
+    <div className="container main-container">
+      <div className="row">
+          <div className="col-lg-8">
+            <h1 className="page-header">About Me</h1>
+            <div className="row">
+              <div className="col-md-3">
+                <img className="img img-thumbnail img-fluid profile-img" src="../../public/assets/images/profile-pic.png" alt="johanna leal pic" />
+              </div>
 
+              <div className="col-md-9">
+                <p>I am a current student at Columbia University Engineering Coding Bootcamp. My most recent experience is as a technical project manager at an educational SaaS startup. I have extensive experience as a software developer but switched over to project management eight years ago. I decided to take this bootcamp to get up to speed on my coding skills. These skills will assist me in my current profession as a technical project manager or perhaps as a web developer.</p>
+
+                  <p>This portfolio showcases some of the homework and projects that I completed during my bootcamp classes. It includes full-stack technologies such as: HTML, CSS, Bootstrap, JavaScript, and node.js. To see my work, click on the GitHub icon on this page.</p>
+              </div>
+            </div>
+          </div>
+    
+          {/* Sidebar - Let's Connect */}
+          <div className="col-lg-4">
+            <div className="card mt-2">
+              <div className="card-body lets-connect">
+                <h4 className="card-title card-center">Let's Connect</h4>
+                <br></br>
+                <p className="card-text">
+                  <a target="_blank" rel="noreferrer" href="https://github.com/johannaleal"><img src="../../public/assets/images/GitHub-Mark-64px.png" alt="GitHub icon" /></a>
+                  &nbsp;&nbsp;
+                  <a target="_blank" rel="noreferrer" href="https://www.linkedin.com/in/johannarleal/"><img src="../../public/assets/images/In-Blue-Logo-original.png" alt="LinkedIn icon" /></a></p>
+                  <a id="resume-link" target="_blank" href="./assets/Leal_Johanna_Resume.pdf">My Resume</a>
+              </div>
+            </div>
+          </div>
+      </div>
+  </div>
+  );
 };
 
 export default About;
