25db72cd168d1a5d58e920997151da0e601ec22f
Johanna L <johannarleal@gmail.com>
2021-03-29 17:00:40 -0400

    Replaced navbar with React navbar

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,30 +1,22 @@
 import React from "react";
+import { Nav, Navbar } from "react-bootstrap";
 import "./style.css";
 
 // Depending on the current path, this component sets the "active" class on the appropriate navigation link item
-function Navbar() {
+function App() {
   return (
-
-    <nav className="navbar main-nav-div navbar-expand-lg navbar-light bg-light">
-      <div className="container">
-        <span className="name-hdr">
-          <a className="navbar-brand" href="/about"><span style={{color: "white"}}>Johanna Leal</span></a>
-        </span>
-
-        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="/navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-          <span className="navbar-toggler-icon"></span>
-        </button>
-
-        <div className="collapse navbar-collapse" id="navbarSupportedContent">
-          <div className="navbar-nav nav-links mr-auto">
-            <a className="nav-link active" aria-current="page" href="/about">About</a>
-            <a className="nav-link" href="/portfolio">Portfolio</a>
-            <a className="nav-link" href="/contact">Contact</a>
-          </div>
-        </div>
-      </div>
-    </nav>
+    <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+    <Navbar.Brand href="#home">Johanna Leal</Navbar.Brand>
+    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+    <Navbar.Collapse id="responsive-navbar-nav">
+      <Nav className="mr-auto">
+        <Nav.Link href="/about">About</Nav.Link>
+        <Nav.Link href="/portfolio">Portfolio</Nav.Link>
+        <Nav.Link href="/contact">Contact</Nav.Link>
+      </Nav>
+    </Navbar.Collapse>
+    </Navbar>
   );
 }
 
-export default Navbar;
+export default App;
